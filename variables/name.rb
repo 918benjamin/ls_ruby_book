1a6ab46f4ca96c79dfcf8b248c87ca1516d13304
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-09-23 13:41:48 -0400

    Finish variables chapter exercises

--- variables/name.rb
+++ variables/name.rb
@@ -1,4 +1,17 @@
+# Exercise 1 section
 print "Type your name: "
 name = gets.chomp
 
 puts "Hey there, #{name}. That's a fine name!"
+
+# Exercise 3 section
+10.times {puts name}
+
+# Exercise 4 section
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "#{first_name} #{last_name}"
