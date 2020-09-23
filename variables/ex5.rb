2d88fbe33888db2653517e2ada9e84a8abb30c5b
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-09-23 13:43:26 -0400

    Add final changes from variables chapter exercises

---
+++ variables/ex5.rb
@@ -0,0 +1,7 @@
+=begin
+The first program outputs x as 3, since x was initialized outside
+the block used by the times method.
+
+The second program throws an error since x is defined within the
+block and then is called outside the block.
+=end
